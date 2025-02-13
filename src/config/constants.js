da4389d5ef87e68124a6e281f08c19d43fb506e8
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-02-13 17:14:03 -0300

    TMDB setup

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,16 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const appPath = fileURLToPath(import.meta.url);
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDAwZGVlMzU3OTg1OWZlNmY0NzEwYjI2YTBhZDc0ZCIsIm5iZiI6MTY1MTA4MDQ5Ny4wODQsInN1YiI6IjYyNjk3ZDMxMTU4Yzg1MzVjMDkxZTEyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SljCeWh1aWHOEABKMu5un_vZV61Co3faEy12hUS-PZo";
 
 export const PORT = 3000;
+export const BASE_URL = "https://api.themoviedb.org/3/movie";
+export const REQ_OPTIONS = {
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
 export const ROOT_DIR = dirname(appPath).replace("/config", "");
 export const VIEWS_DIR = path.join(ROOT_DIR, "views");
