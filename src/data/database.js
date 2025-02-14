fe222381d2b6bceca2e4f719660245d1be69613c
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-02-13 21:46:13 -0300

    Create movies database

---
+++ src/data/database.js
@@ -0,0 +1,150 @@
+const movies = [
+  {
+    id: 1726,
+    name: "Iron Man",
+  },
+  {
+    id: 1724,
+    name: "The Incredible Hulk",
+  },
+  {
+    id: 10138,
+    name: "Iron Man 2",
+  },
+  {
+    id: 10195,
+    name: "Thor",
+  },
+  {
+    id: 1771,
+    name: "Captain America: The First Avenger",
+  },
+  {
+    id: 24428,
+    name: "The Avengers",
+  },
+  {
+    id: 68721,
+    name: "Iron Man 3",
+  },
+  {
+    id: 76338,
+    name: "Thor: The Dark World",
+  },
+  {
+    id: 100402,
+    name: "Captain America: The Winter Soldier",
+  },
+  {
+    id: 118340,
+    name: "Guardians of the Galaxy",
+  },
+  {
+    id: 99861,
+    name: "Avengers: Age of Ultron",
+  },
+  {
+    id: 102899,
+    name: "Ant-Man",
+  },
+  {
+    id: 271110,
+    name: "Captain America: Civil War",
+  },
+  {
+    id: 284052,
+    name: "Doctor Strange",
+  },
+  {
+    id: 283995,
+    name: "Guardians of the Galaxy Vol. 2",
+  },
+  {
+    id: 315635,
+    name: "Spider-Man: Homecoming",
+  },
+  {
+    id: 284053,
+    name: "Thor: Ragnarok",
+  },
+  {
+    id: 284054,
+    name: "Black Panther",
+  },
+  {
+    id: 299536,
+    name: "Avengers: Infinity War",
+  },
+  {
+    id: 363088,
+    name: "Ant-Man and the Wasp",
+  },
+  {
+    id: 299537,
+    name: "Captain Marvel",
+  },
+  {
+    id: 299534,
+    name: "Avengers: Endgame",
+  },
+  {
+    id: 429617,
+    name: "Spider-Man: Far From Home",
+  },
+  {
+    id: 497698,
+    name: "Black Widow",
+  },
+  {
+    id: 566525,
+    name: "Shang-Chi and the Legend of the Ten Rings",
+  },
+  {
+    id: 524434,
+    name: "Eternals",
+  },
+  {
+    id: 634649,
+    name: "Spider-Man: No Way Home",
+  },
+  {
+    id: 453395,
+    name: "Doctor Strange in the Multiverse of Madness",
+  },
+  {
+    id: 616037,
+    name: "Thor: Love and Thunder",
+  },
+  {
+    id: 505642,
+    name: "Black Panther: Wakanda Forever",
+  },
+  {
+    id: 640146,
+    name: "Ant-Man and the Wasp: Quantumania",
+  },
+  {
+    id: 447365,
+    name: "Guardians of the Galaxy Vol. 3",
+  },
+  {
+    id: 609681,
+    name: "The Marvels",
+  },
+  {
+    id: 533535,
+    name: "Deadpool & Wolverine",
+  },
+  {
+    id: 822119,
+    name: "Captain America: Brave New World",
+  },
+  {
+    id: 986056,
+    name: "Thunderbolts*",
+  },
+  {
+    id: 617126,
+    name: "The Fantastic Four: First Steps",
+  },
+];
